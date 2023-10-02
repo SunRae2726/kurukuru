c87a7545fab93fc20a46a22ff7f54948d298dcd0
SunRae2726 <70531287+SunRae2726@users.noreply.github.com>
2023-10-02 03:48:49 -0700

    fixed wallpaper

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 function changeWallpaper(wallpaper) {
     document.querySelector("body").style.background = "url('images/" + wallpaper + "') no-repeat center center fixed";
     document.querySelector("body").style.backgroundSize = "100%";
+    document.querySelector("body").style.backgroundSize = "cover";
     document.getElementById("reset").style.color = '#FFFBD4';
 }
 
